2a0155858c8f5ac1354bb602a02bcec27e8e7949
Minghao2812 <Minghao2812@users.noreply.github.com>
2022-08-08 23:47:33 +0800

    Upload the first finish.

--- LSH.py
+++ LSH.py
@@ -57,19 +57,31 @@ class LSH:
                 onehot.append(0)
         return onehot
 
-    def create_hash_idx(self, digit: 'int' = 20) -> 'list(list(int))':
-        hash_idx_list = []
-        for _ in range(digit):
-            hash_num = list(range(self.vocab_size))
-            random.shuffle(hash_num)
-            hash_idx_list.append(hash_num)
-        return hash_idx_list
+    def create_signature_idx(self, digit: 'int' = 20) -> 'list(list(int))':
+        """This function is a preparation step for the MinHash.
 
-    def minhash(self, onehot, hash_idx_list: 'list(list(int))'):
+        Parameters
+        ----------
+        digit :
+            The digit (length) of the MinHash signature.
+        """
+        sig_idx_list = []
+        for _ in range(digit):
+            sig_idx = list(range(self.vocab_size))
+            random.shuffle(sig_idx)
+            sig_idx_list.append(sig_idx)
+        return sig_idx_list
+
+    def minhash(self, onehot, sig_idx_list: 'list(list(int))'):
+        """For each number in [0, ..., |V|], find it from a sub-list of sig_idx_list, and get its 'index', 
+        then look back to the one-hot vector. If the one-hot vector at that 'index' is 1, keep this 'index' 
+        as a signature digit and continue. As for the next step of this iteration, use the next sub-list of 
+        sig_idx_list.
+        """
         signature = []
-        for hash_idx in range(len(hash_idx_list)):
+        for sub_list in sig_idx_list:
             for num in range(self.vocab_size):
-                idx = hash_idx.index(num)
+                idx = sub_list.index(num)
                 if onehot[idx] == 1:
                     signature.append(idx)
                     break
@@ -88,8 +100,8 @@ class LSH:
         assert len(signature
                    ) % k == 0, 'Length of signature should be divisible by b!'
         banding = []
-        window = len(signature) / k
-        for i in range(len(signature)):
+        window = int(len(signature) / k)
+        for i in range(0, len(signature), window):
             banding.append(signature[i:i + window])
         return banding
 
@@ -98,11 +110,12 @@ class LSH:
     #     b = set(b)
     #     return len(a.intersection(b)) / len(a.union(b))
 
-    def compare(self, *args):
-        banding1, banding2 = args
-        for sub1, sub2 in zip(banding1, banding2):
-            if sub1 == sub2:
+    def compare(self, *bandings):
+        for b1, b2 in zip(*bandings):
+            if b1 == b2:
                 return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
@@ -130,13 +143,26 @@ if __name__ == '__main__':
     print('One-hot2: ', onehot2)
     print('One-hot3: ', onehot3)
 
-    hash_idx_list = model.create_hash_idx(20)
+    hash_idx_list = model.create_signature_idx(20)
     print('Hash indices: ', hash_idx_list)
     print('Hash indices size: ', model.vocab_size * len(hash_idx_list))
 
-    # sig1 = create_signature(onehot1, hash_num, len(vocab))
-    # sig2 = create_signature(onehot2, hash_num, len(vocab))
-    # sig3 = create_signature(onehot3, hash_num, len(vocab))
-    # print('Signature1: ', sig1)
-    # print('Signature2: ', sig2)
-    # print('Signature3: ', sig3)
+    sig1 = model.minhash(onehot1, hash_idx_list)
+    sig2 = model.minhash(onehot2, hash_idx_list)
+    sig3 = model.minhash(onehot3, hash_idx_list)
+    print('Signature1: ', sig1)
+    print('Signature1 size: ', len(sig1))
+    print('Signature2: ', sig2)
+    print('Signature2 size: ', len(sig2))
+    print('Signature3: ', sig3)
+    print('Signature3 size: ', len(sig3))
+
+    banding1 = model.banding(sig1, 5)
+    banding2 = model.banding(sig2, 5)
+    banding3 = model.banding(sig3, 5)
+    print('Banding1 :', banding1)
+    print('Banding2 :', banding2)
+    print('Banding3 :', banding3)
+
+    print(model.compare(banding1, banding2))
+    print(model.compare(banding2, banding3))
